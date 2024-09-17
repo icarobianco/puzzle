3b87ca5a7c2d15a81ca8c83051e67bbb566ddf42
icarobianco <icaro@interligados.net>
2024-09-17 19:32:07 -0300

    Initial commit...

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,40 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
+
+import mergeImages from 'merge-images';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  template: `
-    <h1>Hello from {{ name }}!</h1>
-    <a target="_blank" href="https://angular.dev/overview">
-      Learn more about Angular
-    </a>
-  `,
+  imports: [
+    CommonModule,
+  ],
+  templateUrl: './app.component.html'
 })
 export class App {
-  name = 'Angular';
+
+  name = 'Puzzle';
+  data:any = {
+    img: ''
+  }
+
+  onFileSelected(event: Event): void {
+    console.error('CreateComponent -> onFileSelected');
+
+    const input = event.target as HTMLInputElement
+    if (input && input.files && input.files.length) {
+      const file = input.files[0];
+      const reader = new FileReader()
+      reader.onload = () => {
+        this.data.img = reader.result
+
+        // mergeImages(['/assets/knife.png', '/eyes.png']).then((b64: any) => this.data.src = b64);
+      };
+      reader.readAsDataURL(file)
+      // reader.readAsArrayBuffer(file);
+    }
+  }
 }
 
 bootstrapApplication(App);
